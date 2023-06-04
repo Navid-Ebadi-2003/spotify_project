0fd2978e3317895a71b7458fa1a32040ab261dfd
navid ebadi <navid.ebadi.2003@gmail.com>
2023-06-04 14:57:16 +0330

    login page

---
+++ src/main/java/Client/controllers/signIn.java
@@ -0,0 +1,16 @@
+package Client.controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class signIn extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+    public void start(Stage primaryStage){
+
+
+
+    }
+}
