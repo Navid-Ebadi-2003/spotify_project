c5e66dc81f0f56b6cc91a3e6ddaeb4d0738a26b7
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-13 00:56:25 +0330

    AlbumBoxController + ArtistBoxController + PlaylistMainBoxController + AlbumBoxController + UserBoxController

--- src/main/java/Client/Controllers/Boxes/ArtistBox/ArtistBoxController.java
+++ src/main/java/Client/Controllers/Boxes/ArtistBox/ArtistBoxController.java
@@ -1,4 +1,13 @@
 package Client.Controllers.Boxes.ArtistBox;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
 public class ArtistBoxController {
+    @FXML
+    private HBox artistHBox;
+
+    @FXML
+    private ImageView artistPicture;
 }
--- src/main/java/Client/Controllers/Boxes/PlaylistMainBox/PlaylistMainBoxController.java
+++ src/main/java/Client/Controllers/Boxes/PlaylistMainBox/PlaylistMainBoxController.java
@@ -1,4 +1,17 @@
 package Client.Controllers.Boxes.PlaylistMainBox;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
 public class PlaylistMainBoxController {
+    @FXML
+    private HBox creatorHBox;
+
+    @FXML
+    private ImageView playlistPicture;
+
+    @FXML
+    private Label playlistTitle;
 }
