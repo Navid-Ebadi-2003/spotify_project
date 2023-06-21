56b290e7c904fdcaf86bbda715830aafdddcc4a9
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-22 03:09:35 +0330

    changed width and height of mainPage + bottom musicPlayer, completed secondMusicBox and it's controller

--- src/main/java/Client/Controllers/Boxes/MusicSecondBox/MusicSecondBoxController.java
+++ src/main/java/Client/Controllers/Boxes/MusicSecondBox/MusicSecondBoxController.java
@@ -1,4 +1,16 @@
 package Client.Controllers.Boxes.MusicSecondBox;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.ImageView;
+
 public class MusicSecondBoxController {
+    @FXML
+    private Hyperlink artistsHyperLink;
+
+    @FXML
+    private ImageView trackPicture;
+
+    @FXML
+    private Hyperlink trackTitle;
 }
