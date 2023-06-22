cc6100676dd0846a41402b885b83b5f6cbf3a921
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-22 15:49:10 +0330

    designated lyrics page

--- src/main/java/Client/Controllers/LyricsPage/LyricsPageController.java
+++ src/main/java/Client/Controllers/LyricsPage/LyricsPageController.java
@@ -1,4 +1,10 @@
 package Client.Controllers.LyricsPage;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class LyricsPageController {
+    @FXML
+    private Text lyricsText;
+
 }
