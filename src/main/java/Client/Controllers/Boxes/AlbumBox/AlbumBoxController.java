4b6794c12deeb1ef53de0fcc51dbb0f1148c8850
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-23 02:14:56 +0330

    k, v method done on album and artist boxes (ask mmd)

--- src/main/java/Client/Controllers/Boxes/AlbumBox/AlbumBoxController.java
+++ src/main/java/Client/Controllers/Boxes/AlbumBox/AlbumBoxController.java
@@ -1,19 +1,33 @@
 package Client.Controllers.Boxes.AlbumBox;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class AlbumBoxController {
     @FXML
     private ImageView albumPicture;
 
     @FXML
-    private Label albumTitle;
+    private Hyperlink albumTitleHyperLink;
 
     @FXML
     private HBox artistHBox;
+    // This is for sending request if a user clicks on hyperlink
+    private UUID albumId;
+    // This stores artistName, artistId
+    private HashMap<String, UUID> artists;
+
+    @FXML
+    public void goToAlbumPage(ActionEvent event) {
+
+    }
 
     /*
         setter and getters
@@ -27,12 +41,12 @@ public class AlbumBoxController {
         this.albumPicture = albumPicture;
     }
 
-    public Label getAlbumTitle() {
-        return albumTitle;
+    public Hyperlink getAlbumTitleHyperLink() {
+        return albumTitleHyperLink;
     }
 
-    public void setAlbumTitle(Label albumTitle) {
-        this.albumTitle = albumTitle;
+    public void setAlbumTitleHyperLink(Hyperlink albumTitleHyperLink) {
+        this.albumTitleHyperLink = albumTitleHyperLink;
     }
 
     public HBox getArtistHBox() {
@@ -41,5 +55,21 @@ public class AlbumBoxController {
 
     public void setArtistHBox(HBox artistHBox) {
         this.artistHBox = artistHBox;
+    }
+
+    public UUID getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(UUID albumId) {
+        this.albumId = albumId;
+    }
+
+    public HashMap<String, UUID> getArtists() {
+        return artists;
+    }
+
+    public void setArtists(HashMap<String, UUID> artists) {
+        this.artists = artists;
     }
 }
