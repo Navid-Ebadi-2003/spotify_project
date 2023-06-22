247e71bb8d65b6cd2bbf925fd34e2aa30ef96876
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-22 14:59:15 +0330

    added follow to playlist

--- src/main/java/Client/Controllers/PlaylistPage/PlaylistController.java
+++ src/main/java/Client/Controllers/PlaylistPage/PlaylistController.java
@@ -1,17 +1,31 @@
 package Client.Controllers.PlaylistPage;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 public class PlaylistController {
     @FXML
     private ScrollPane bottomScrollPane;
 
     @FXML
+    private Button likeButton;
+
+    @FXML
     private ImageView playlistPicture;
 
     @FXML
     private Label playlistTitle;
+
+    @FXML
+    private VBox tracksVbox;
+
+    @FXML
+    void like_unlike(ActionEvent event) {
+
+    }
 }
