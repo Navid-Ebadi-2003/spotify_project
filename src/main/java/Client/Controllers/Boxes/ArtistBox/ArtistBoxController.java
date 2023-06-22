9eca2d61044763c9499f603cf7195e54ea6f9127
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-22 16:14:43 +0330

    revise on artist-page :/

--- src/main/java/Client/Controllers/Boxes/ArtistBox/ArtistBoxController.java
+++ src/main/java/Client/Controllers/Boxes/ArtistBox/ArtistBoxController.java
@@ -1,15 +1,32 @@
 package Client.Controllers.Boxes.ArtistBox;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
-import java.awt.*;
 
 public class ArtistBoxController {
     @FXML
-    private ImageView userPicture;
+    private HBox albumsHbox;
 
     @FXML
-    private Label username;
+    private Text artistAboutText;
+
+    @FXML
+    private Label artistName;
+
+    @FXML
+    private ImageView artistPicture;
+
+    @FXML
+    private Button followButton;
+
+    @FXML
+    void toggleFollow(ActionEvent event) {
+
+    }
 }
