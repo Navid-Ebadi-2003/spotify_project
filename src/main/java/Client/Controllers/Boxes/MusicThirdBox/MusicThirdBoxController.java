d08c5360afb5334c2d221caf5a0f4f0f57be2f4e
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-22 14:20:46 +0330

    designated third box of music

--- src/main/java/Client/Controllers/Boxes/MusicThirdBox/MusicThirdBoxController.java
+++ src/main/java/Client/Controllers/Boxes/MusicThirdBox/MusicThirdBoxController.java
@@ -1,4 +1,17 @@
 package Client.Controllers.Boxes.MusicThirdBox;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
 public class MusicThirdBoxController {
+    @FXML
+    private HBox artistsHbox;
+
+    @FXML
+    private ImageView trackPicture;
+
+    @FXML
+    private Label trackTitle;
 }
