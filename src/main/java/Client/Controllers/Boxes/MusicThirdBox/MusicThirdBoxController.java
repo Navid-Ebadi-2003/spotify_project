d3a93722a8ad23197a3209418a96a13bf7c21830
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-22 21:29:48 +0330

    added new set of attributes and functionalities to third box of music class

--- src/main/java/Client/Controllers/Boxes/MusicThirdBox/MusicThirdBoxController.java
+++ src/main/java/Client/Controllers/Boxes/MusicThirdBox/MusicThirdBoxController.java
@@ -1,6 +1,8 @@
 package Client.Controllers.Boxes.MusicThirdBox;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -10,8 +12,19 @@ public class MusicThirdBoxController {
     private HBox artistsHbox;
 
     @FXML
+    private Button downPlayTrack;
+
+    @FXML
+    private ImageView downloadPlayIcon;
+
+    @FXML
     private ImageView trackPicture;
 
     @FXML
     private Label trackTitle;
+
+    @FXML
+    void downPlay(ActionEvent event) {
+
+    }
 }
