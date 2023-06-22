31a965f900669c503e0274dd1f921b06bcca03d9
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-22 14:01:40 +0330

    designated playlistPage

--- src/main/java/Client/Controllers/PlaylistPage/PlaylistController.java
+++ src/main/java/Client/Controllers/PlaylistPage/PlaylistController.java
@@ -1,4 +1,17 @@
 package Client.Controllers.PlaylistPage;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+
 public class PlaylistController {
+    @FXML
+    private ScrollPane bottomScrollPane;
+
+    @FXML
+    private ImageView playlistPicture;
+
+    @FXML
+    private Label playlistTitle;
 }
