c004c42a652e5f0677801812250364d9559274d3
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-22 15:13:05 +0330

    designated album page

--- src/main/java/Client/Controllers/AlbumPage/AlbumPageController.java
+++ src/main/java/Client/Controllers/AlbumPage/AlbumPageController.java
@@ -1,4 +1,21 @@
 package Client.Controllers.AlbumPage;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
 public class AlbumPageController{
+    @FXML
+    private ImageView albumPicture;
+
+    @FXML
+    private Label albumTitle;
+
+    @FXML
+    private ScrollPane bottomScrollPane;
+
+    @FXML
+    private VBox tracksVbox;
 }
