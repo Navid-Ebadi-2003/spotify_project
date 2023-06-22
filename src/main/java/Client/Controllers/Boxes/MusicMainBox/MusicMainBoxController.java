3865c7e8f2bebbde9c8f5c7e382d21b5a19d3230
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-22 20:59:39 +0330

    a review on main box of music

--- src/main/java/Client/Controllers/Boxes/MusicMainBox/MusicMainBoxController.java
+++ src/main/java/Client/Controllers/Boxes/MusicMainBox/MusicMainBoxController.java
@@ -1,6 +1,8 @@
 package Client.Controllers.Boxes.MusicMainBox;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -14,6 +16,11 @@ public class MusicMainBoxController {
     private ImageView trackPicture;
 
     @FXML
-    private Label trackTitle;
+    private Hyperlink trackTitle;
+
+    @FXML
+    void goToAlbumPage(ActionEvent event) {
+
+    }
 
 }
