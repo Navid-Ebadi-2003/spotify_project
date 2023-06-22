0db08af339d01795f7e1151a61dacb68a918949e
Mohammad Hossein Basouli <125287952+MMDPROJECT@users.noreply.github.com>
2023-06-22 03:30:19 +0330

    rebuilt of homePage scene and controller class

--- src/main/java/Client/Controllers/HomePage/HomePageController.java
+++ src/main/java/Client/Controllers/HomePage/HomePageController.java
@@ -1,9 +1,16 @@
 package Client.Controllers.HomePage;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
 public class HomePageController {
     @FXML
-    private HBox tracksHBox;
+    private AnchorPane homePageAnchorPane;
+
+    @FXML
+    private HBox likedMusicsHbox;
+
+    @FXML
+    private HBox suggestedMusicsHbox;
 }
